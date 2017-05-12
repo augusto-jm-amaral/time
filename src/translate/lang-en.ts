3738affef0aaa088df27ccdafa63272902b6f844
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-05-12 15:50:56 -0300

    Translate all strings

--- src/translate/lang-en.ts
+++ src/translate/lang-en.ts
@@ -2,5 +2,14 @@ export const LANG_EN_NAME = 'en';
 export const LANG_EN_TRANS = {
   'Tasks': 'Tasks',
   'Configuration': 'Configuration',
-  'Language': 'Language'
+  'Language': 'Language',
+  'Theme': 'Theme',
+  'Name': 'Name',
+  'Description': 'Description',
+  'Create': 'Create',
+  'Color': 'Color',
+  'Pause': 'Pause',
+  'Time': 'Time',
+  'History': 'History',
+  'Color List': 'Color List'
 };
--- src/translate/lang-pt.ts
+++ src/translate/lang-pt.ts
@@ -2,5 +2,14 @@ export const LANG_PT_NAME = 'pt';
 export const LANG_PT_TRANS = {
   'Tasks': 'Tarefas',
   'Configuration': 'Configurações',
-  'Language': 'Linguagem'
+  'Language': 'Linguagem',
+  'Theme': 'Tema',
+  'Name': 'Nome',
+  'Description': 'Description',
+  'Create': 'Criar',
+  'Color': 'Cor',
+  'Pause': 'Pausa',
+  'Time': 'Tempo',
+  'History': 'Histórico',
+  'Color List': 'Lista de cores'
 };
